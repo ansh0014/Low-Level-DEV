b01b4bca85e78cb4b5019ecf2d9d87a18290b299
ansh0014 <anshul.jagota@yahoo.com>
2026-04-21 20:01:49 +0530

    learning about the mutex in c++ and how to use it in a simple way to avoid

---
+++ Mutex/t.cpp
@@ -0,0 +1,25 @@
+// #include<iostream>
+// #include<thread>
+// #include<mutex>
+// #include<windows.h>
+// using namespace std;
+// int counter=0;
+// std::mutex mtx;
+// void increment_counter(){
+//     for(int i=0;i<100000;i++){
+//         // mtx.lock();
+//         // counter++;
+//         // mtx.unlock();
+// // this is create the deadlock 
+// std::lock_guard<std::mutex> lock(mtx);
+// counter++;
+//     }
+// }
+// int main(){
+//     thread t1(increment_counter);
+//     thread t2(increment_counter);
+//     t1.join();
+//     t2.join();
+//     cout<<"Final counter value: "<<counter<<endl;
+
+// }
